bb552ae40a1789615551a0a27aaaaccbdf07cb0a
MAKHTAR WADE <makhtar2gsm@gmail.com>
2026-05-18 15:26:00 +0000

    config: update supabase hostname for images

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'mpgjgsojeezqiovtkepz.supabase.co',
+                hostname: 'wrjnybgysqjcqnwxlhld.supabase.co',
                 port: '',
                 pathname: '/storage/v1/object/public/**',
             },
